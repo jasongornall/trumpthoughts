14d07c97316d4178c280bbbc0035b45848a9f579
Jason Gornall <jason.gornall@lessthan3.com>
2017-02-19 15:50:44 -0800

    trump thoughts

--- javascripts/main.js
+++ javascripts/main.js
@@ -6,9 +6,3 @@ msnry = null;
 ref = new Firebase("https://question-everything.firebaseio.com");
 
 links_ref = new Firebase("https://question-links.firebaseio.com");
-
-$('textarea').trumbowyg({
-  removeformatPasted: true,
-  autogrow: true,
-  btns: [['formatting'], 'btnGrp-semantic', ['superscript', 'subscript'], ['link'], 'btnGrp-justify', 'btnGrp-lists', ['horizontalRule'], ['removeformat'], ['fullscreen']]
-});
