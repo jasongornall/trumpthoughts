161ab3d478c86942486a61879c2ba28fccb0e86e
Jason Gornall <jason.gornall@lessthan3.com>
2017-02-19 16:36:04 -0800

    trump thoughts

--- javascripts/main.js
+++ javascripts/main.js
@@ -6,3 +6,5 @@ msnry = null;
 ref = new Firebase("https://question-everything.firebaseio.com");
 
 links_ref = new Firebase("https://question-links.firebaseio.com");
+
+$('trix-editor')[0].editor.setSelectedRange([100, 100]);
